f4fa781948a4dff62ccdea490f71b351097c4a9c
Kevin Knott <KKnoTT93@gmail.com>
2020-09-04 17:27:19 -0500

    Making passport work with Google only first part of flow

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send({ hi: 'there' });
-});
+//  Make sure that we are using passport specifically with Google OAuth2.0
+//  Passport.use can be added multiple times for different strategies
+// const callbackURI = "http://localhost:" + process.env.PORT + "/auth/google/callback";
+passport.use(
+    new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    }, (accessToken) => { console.log(accessToken); })
+);
+
+
+// app.get("/", (req, res) => {
+//     res.send({ hi: 'there' });
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
