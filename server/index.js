367562d0b07d6472c4249b6951fdd4c17d3bcedc
Kevin Knott <KKnoTT93@gmail.com>
2020-09-09 17:48:29 -0500

    Added simple mongo db connection with mongoose

--- server/index.js
+++ server/index.js
@@ -2,6 +2,28 @@ const express = require('express');
 require('dotenv').config();
 require('./services/passport');
 
+const mongoose = require('mongoose');
+
+const uri =
+    'mongodb+srv://' +
+    process.env.ATLAS_USER +
+    ':' +
+    process.env.ATLAS_PASS +
+    '@first-cluster.r9nhd.mongodb.net/' +
+    process.env.ATLAS_DB +
+    '?retryWrites=true&w=majority';
+
+mongoose.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}, function (err) {
+    if (err) {
+        console.log("Unable to connect to DB", err);
+    } else {
+        console.log("Connected to DB");
+    }
+});
+
 const app = express();
 require('./routes/authRoutes')(app);
 
