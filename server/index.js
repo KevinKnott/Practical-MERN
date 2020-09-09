c0a33f2a27028f6958827afff0ce4c9ba67ff10c
Kevin Knott <KKnoTT93@gmail.com>
2020-09-09 18:46:11 -0500

    Fixing authRoutes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('dotenv').config();
 require('./models/User');
 require('./services/passport');
 
 
-const mongoose = require('mongoose');
 
 const uri =
     'mongodb+srv://' +
@@ -27,6 +29,14 @@ mongoose.connect(uri, {
 });
 
 const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [process.env.COOKIE_KEY]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 // app.get("/", (req, res) => {
