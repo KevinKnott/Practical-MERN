53f2bc647ca79e5198490d932153c7e80c2711c9
Kevin Knott <KKnoTT93@gmail.com>
2020-09-11 18:23:36 -0500

    Add in a redux store for use throughout our componenets

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
