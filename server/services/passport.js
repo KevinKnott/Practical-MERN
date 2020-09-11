316332785e45330b6b0261b6d08a721b58e4bd18
Kevin Knott <KKnoTT93@gmail.com>
2020-09-10 21:26:48 -0500

    Add proxy setting so in production google redirect uses https

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,7 +23,8 @@ passport.use(
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "/auth/google/callback",
-        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+        proxy: true
     }, (accessToken, refreshToken, profile, done) => {
         // console.log(profile.id);
         const potentialUser = { googleId: profile.id };
