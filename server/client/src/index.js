c365e70dcb3be51faae281e2c9b24ca2aac5426e
Kevin Knott <KKnoTT93@gmail.com>
2020-09-11 18:14:07 -0500

    Create basic Index.js with basic App component

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
