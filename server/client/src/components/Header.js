5cca053af7d24838d0c03121a16a8dd576e8fd98
Kevin Knott <KKnoTT93@gmail.com>
2020-09-16 19:35:07 -0500

    Figure out state handling in header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return 'Still deciding';
+            case false:
+                return 'im loggedout';
+            default:
+                return 'im logged in';
+
+        }
+    }
+
     render() {
+        // console.log(this.props);
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a className="left brand-logo" href="/">Emaily</a>
                     <ul className="right">
-                        <a href="/auth/google"> Login with Google</a>
+                        {/* <a href="/auth/google"> Login with Google</a> */}
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -16,4 +31,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);
