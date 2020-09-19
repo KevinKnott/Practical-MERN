4a47076e574c56183969fcad91da7670f2a03765
Kevin Knott <KKnoTT93@gmail.com>
2020-09-18 19:53:36 -0500

    Use a ternary operator to link to Surveys or Home based off user login status

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -15,7 +16,6 @@ class Header extends Component {
                 return (
                     <li><a href="/api/logout">Logout</a>/</li>
                 );
-
         }
     }
 
@@ -24,7 +24,7 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo" href="/">Emaily</a>
+                    <Link className="left brand-logo" to={this.props.user ? '/surveys' : '/'}>Emaily</Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
